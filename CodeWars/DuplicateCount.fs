76ea5648e231ba068832c17aa7c34119121675cb
solomkin <solomkin@skbkontur.ru>
2023-01-15 15:28:04 +0500

    Added duplicateCount kata

---
+++ CodeWars/DuplicateCount.fs
@@ -0,0 +1,12 @@
+namespace CodeWars
+
+open System
+
+module DuplicateCount =
+
+    let duplicateCount (input: string) =
+        input
+        |> Seq.groupBy (fun ch -> Char.ToLower ch) // (fun ch -> Char.uppercase ch)
+        |> Seq.map (fun (k, v) -> (k, Seq.length v))
+        |> Seq.filter (fun (k, v) -> v > 1)
+        |> Seq.length
---
+++ CodeWarsTests/DuplicateCountTests.fs
@@ -0,0 +1,28 @@
+module DuplicateCountTests
+
+open NUnit.Framework
+open FsUnit
+open CodeWars.DuplicateCount
+
+let lowercase, uppercase = "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+let duplicateCountTestCases =
+    seq {
+        yield TestCaseData("abcde", 0)
+        yield TestCaseData("abcdeaa", 1)
+        yield TestCaseData("abcdeaB", 2)
+        yield TestCaseData("abcdeaB", 2)
+        yield TestCaseData("Indivisibilities", 2)
+        yield TestCaseData(lowercase, 0)
+        yield TestCaseData(lowercase + "aaAb", 2)
+        yield TestCaseData(lowercase + lowercase, 26)
+        yield TestCaseData(lowercase + uppercase, 26)
+    }
+
+[<TestCaseSource("duplicateCountTestCases")>]
+let ``Should equal to`` (input: string, expected: int) =
+    let actual = duplicateCount input
+    actual |> should equal expected
+
+
+
